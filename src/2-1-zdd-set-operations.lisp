3b3039c8ee9e89a19fd67929b470067e95f19cca
Masataro Asai <guicho2.71828@gmail.com>
2017-03-13 15:41:19 +0900

    more operations

--- src/2-1-zdd-set-operations.lisp
+++ src/2-1-zdd-set-operations.lisp
@@ -12,7 +12,7 @@
       ;; CUDD-ZDD-DAG-SIZE
       ;; CUDD-ZDD-DIFF
       CUDD-ZDD-DIFF-CONST
-      CUDD-ZDD-DIVIDE                   ; unate version of CUDD-ZDD-WEAK-DIV
+      ;; CUDD-ZDD-DIVIDE                   ; unate version of CUDD-ZDD-WEAK-DIV
       ;; CUDD-ZDD-DIVIDE-F                 ; obsoleted version of CUDD-ZDD-DIVIDE
       ;; CUDD-ZDD-DUMP-DOT --- test
       CUDD-ZDD-FIRST-PATH
@@ -27,7 +27,7 @@
       CUDD-ZDD-PRINT-DEBUG
       CUDD-ZDD-PRINT-MINTERM
       CUDD-ZDD-PRINT-SUBTABLE
-      CUDD-ZDD-PRODUCT                  ; binate version of CUDD-ZDD-UNATE-PRODUCT
+      ;; CUDD-ZDD-PRODUCT                  ; binate version of CUDD-ZDD-UNATE-PRODUCT
       CUDD-ZDD-READ-NODE-COUNT
       CUDD-ZDD-REALIGN-DISABLE
       CUDD-ZDD-REALIGN-ENABLE
@@ -37,10 +37,10 @@
       ;; CUDD-ZDD-SUBSET-0
       ;; CUDD-ZDD-SUBSET-1
       CUDD-ZDD-SYMM-PROFILE
-      CUDD-ZDD-UNATE-PRODUCT            ; unate version of CUDD-ZDD-PRODUCT
+      ;; CUDD-ZDD-UNATE-PRODUCT            ; unate version of CUDD-ZDD-PRODUCT
       ;; CUDD-ZDD-UNION
       ;; CUDD-ZDD-VARS-FROM-BDD-VARS
-      CUDD-ZDD-WEAK-DIV                 ; binate version of CUDD-ZDD-WEAK-DIV
+      ;; CUDD-ZDD-WEAK-DIV                 ; binate version of CUDD-ZDD-DIVIDE
       ;; CUDD-ZDD-WEAK-DIV-F               ; obsoleted version of CUDD-ZDD-WEAK-DIV
       ))
 
@@ -75,4 +75,14 @@
   :zdd "Computes the intersection of F and G.")
 (def-cudd-call zdd-difference ((:zdd cudd-zdd-diff) (f :node) (g :node))
   :zdd "Computes the difference of F and G.")
+
+(def-cudd-call zdd-divide-unate ((:zdd cudd-zdd-divide) (f :node) (g :node))
+  :zdd "Computes the weak division of F by G (assumes unate representation).")
+(def-cudd-call zdd-divide-binate ((:zdd cudd-zdd-weak-div) (f :node) (g :node))
+  :zdd "Computes the weak division of F by G (assumes binate representation).")
+(def-cudd-call zdd-product-unate ((:zdd cudd-zdd-unate-product) (f :node) (g :node))
+  :zdd "Computes the weak division of F by G (assumes binate representation).")
+(def-cudd-call zdd-product-binate ((:zdd cudd-zdd-product) (f :node) (g :node))
+  :zdd "Computes the weak division of F by G (assumes binate representation).")
+
 
