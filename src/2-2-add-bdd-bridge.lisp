74d1ea572eb327e8168078ac2081a91afd3856bc
Masataro Asai <guicho2.71828@gmail.com>
2017-03-18 04:51:05 +0900

    add->bdd-XXX: generic functions to functions

--- src/2-2-add-bdd-bridge.lisp
+++ src/2-2-add-bdd-bridge.lisp
@@ -2,49 +2,45 @@
 
 (defun bdd->add (bdd)
   "Converts a BDD to a 0-1 ADD"
-  (assert (typep bdd 'bdd-node))
+  (declare (bdd-node bdd))
   (wrap-and-finalize
    (cudd-bdd-to-add (manager-pointer *manager*) (node-pointer bdd))
    'add-node))
 
 (defun add->bdd (add)
   "Converts an ADD to a BDD by replacing all discriminants different from 0 with 1."
-  (assert (typep add 'add-node))
+  (declare (add-node add))
   (wrap-and-finalize
    (cudd-add-bdd-pattern (manager-pointer *manager*) (node-pointer add))
    'bdd-node))
 
-(def-cudd-call add->bdd-interval (() (f :node) lower upper)
-  :generic "Converts an ADD to a BDD by replacing all discriminants greater than or equal to lower and less
-  than or equal to upper with 1, and all other discriminants with 0.")
-
-(defmethod add->bdd-interval ((f add-node) lower upper)
+(defun add->bdd-interval (add lower upper)
+  "Converts an ADD to a BDD by replacing all discriminants greater than or equal to lower and less
+  than or equal to upper with 1, and all other discriminants with 0."
+  (declare (add-node add))
   (wrap-and-finalize (cudd-add-bdd-interval
                       (manager-pointer *manager*)
-                      (node-pointer f)
+                      (node-pointer add)
                       lower upper)
                      'bdd-node))
 
-
-(def-cudd-call add->bdd-strict-threshold (() (f :node) threshold)
-  :generic "Converts an ADD to a BDD by replacing all discriminants STRICTLY greater than value with 1, and
-  all other discriminants with 0.")
-
-(defmethod add->bdd-strict-threshold ((f add-node) threshold)
+(defun add->bdd-strict-threshold (add threshold)
+  "Converts an ADD to a BDD by replacing all discriminants STRICTLY greater than value with 1, and
+  all other discriminants with 0."
+  (declare (add-node add))
   (wrap-and-finalize (cudd-add-bdd-strict-threshold
                       (manager-pointer *manager*)
-                      (node-pointer f)
+                      (node-pointer add)
                       threshold)
                      'bdd-node))
 
-(def-cudd-call add->bdd-threshold (() (f :node) threshold)
-  :generic "Converts an ADD to a BDD by replacing all discriminants greater than or equal to value with 1, and
-  all other discriminants with 0.")
-
-(defmethod add->bdd-threshold ((f add-node) threshold)
+(defun add->bdd-threshold (add threshold)
+  "Converts an ADD to a BDD by replacing all discriminants greater than or equal to value with 1, and
+  all other discriminants with 0."
+  (declare (add-node add))
   (wrap-and-finalize (cudd-add-bdd-threshold
                       (manager-pointer *manager*)
-                      (node-pointer f)
+                      (node-pointer add)
                       threshold)
                      'bdd-node))
 
