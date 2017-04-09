054618cdf4fb1d370b7d4933f1da19e60638565c
Masataro Asai <guicho2.71828@gmail.com>
2017-04-09 19:02:12 +0900

    [ci skip] doc abstract

--- src/2-1-generic-complex.lisp
+++ src/2-1-generic-complex.lisp
@@ -2,24 +2,23 @@
 
 (def-cudd-call exist-abstract ((:add cudd-add-exist-abstract :bdd cudd-bdd-exist-abstract)
                                (f :node) (cube :node))
-  :generic "Abstracts all the variables in cube from f by summing over all
-  possible values taken by the variables. Returns the abstracted
-  DD.")
+  :generic "Abstracts all the variables in CUBE from the DD F by
+  taking the sum over all possible values taken by the variables.
+  Returns the abstracted DD.")
 
 (def-cudd-call or-abstract ((:add cudd-add-or-abstract :bdd cudd-bdd-exist-abstract)
                             (f :node) (cube :node))
-  :generic "Abstracts all the variables in cube from the 0-1 ADD f by
-  taking the disjunction over all possible values taken by the
-  variables.
+  :generic "Abstracts all the variables in CUBE from the DD F by
+taking the disjunction over all possible values taken by the variables.
+Returns the abstracted DD.
 
-If abstracting an ADD, we assume that it is an 0-1-ADD
-")
+If abstracting an ADD, we assume that it is an 0-1-ADD.")
 
 (def-cudd-call univ-abstract ((:add cudd-add-univ-abstract :bdd cudd-bdd-univ-abstract)
                               (f :node) (cube :node))
-  :generic "Abstracts all the variables in cube from the DD f by
-  taking the conjunction over all possible values taken by the
-  variables.
+  :generic "Abstracts all the variables in CUBE from the DD F by
+taking the conjunction over all possible values taken by the variables.
+Returns the abstracted DD.
 
 If abstracting an ADD, we assume that it is an 0-1-ADD")
 
