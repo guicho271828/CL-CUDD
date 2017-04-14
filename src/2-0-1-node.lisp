9c40310a85baaa65012235f4705edf41bea765cc
Masataro Asai <guicho2.71828@gmail.com>
2017-04-15 00:02:30 +0900

    move wrap-and-finalize

--- src/2-0-1-node.lisp
+++ src/2-0-1-node.lisp
@@ -49,6 +49,39 @@ every node in the body and decreasing it after the body is run"
                   :for binding :in pointers
                   :collect `(cudd-recursive-deref ,manager ,(car binding)))))))))
 
+(declaim (inline wrap-and-finalize))
+(defun wrap-and-finalize (pointer type &optional (finalize t))
+  "Wrap the given pointer in a node-node of type TYPE.
+Set the finalizer to call cudd-recursive-deref."
+  (declare (foreign-pointer pointer)
+           ((member bdd-node add-node zdd-node) type))
+  (let ((node (ecase type
+                (bdd-node (make-bdd-node :pointer pointer))
+                (add-node (make-add-node :pointer pointer))
+                (zdd-node (make-zdd-node :pointer pointer)))))
+    (when finalize
+      (trivial-garbage:finalize
+       node
+       (let ((manager *manager*))
+         ;; NOTE: ^^^ This holds the reference from the __finalizer function__ to
+         ;; the manager (along with avoiding problems related to dynamic binding).
+         ;; Without it, the finalizer may be called after the manager is finalized
+         ;; (i.e. cudd-quit is called), invalidating the pointer to the node.
+         ;; It is insufficient to reference a manager from a node, since the order
+         ;; to call finalizers is unspecified. If a manager and a node is freed in
+         ;; the same gc, it could be possible that cudd-quit is called
+         ;; first. Manager object should be referenced until the node finalizer
+         ;; is called.
+         (lambda ()
+           (let ((mp (manager-pointer manager)))
+             (when (zerop (cudd-node-ref-count mp pointer))
+               (error "Tried to decrease reference count of node that already has refcount zero"))
+             (ecase type
+               (bdd-node (cudd-recursive-deref mp pointer))
+               (add-node (cudd-recursive-deref mp pointer))
+               (zdd-node (cudd-recursive-deref-zdd mp pointer))))))))
+    node))
+
 (defmethod print-object ((object node) stream)
   (print-unreadable-object (object stream :type (type-of object) :identity nil)
     (format stream "INDEX ~A " (cudd-node-read-index (node-pointer object)))
@@ -80,7 +113,6 @@ only if their pointers are the same."
   ;; Make sure that we only try to read the value of a constant node
   (assert (node-constant-p node))
   (cudd-node-value %mp% (node-pointer node)))
-
 
 (defstruct (bdd-node (:include node))
   "Node of a binary decision diagram (BDD)")
