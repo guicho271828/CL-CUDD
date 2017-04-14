4714f5d31fc5ce17a8023c5b52115936d70f8656
Masataro Asai <guicho2.71828@gmail.com>
2017-04-14 16:03:36 +0900

    enable manager GC

--- src/2-0-0-manager.lisp
+++ src/2-0-0-manager.lisp
@@ -14,7 +14,28 @@ Every function in this package works with this manager.")
 
 (define-symbol-macro %mp% (manager-pointer *manager*))
 
-(defmacro with-manager ((&key
+(defun manager-init (&key
+                       (initial-num-vars 0)
+                       (initial-num-vars-z 0)
+                       (initial-num-slots 256)
+                       (cache-size 262144)
+                       (max-memory 0))
+  (let* ((p (cudd-init initial-num-vars
+                       initial-num-vars-z
+                       initial-num-slots
+                       cache-size
+                       max-memory))
+         (m (make-manager :pointer p)))
+    (cudd-add-hook p (callback before-gc-hook) :cudd-pre-gc-hook)
+    (cudd-add-hook p (callback after-gc-hook) :cudd-post-gc-hook)
+    (cudd-add-hook p (callback before-gc-hook) :cudd-pre-reordering-hook)
+    (cudd-add-hook p (callback after-gc-hook) :cudd-post-reordering-hook)
+    (tg:finalize m (lambda () (cudd-quit p)))
+    m))
+
+(defmacro with-manager ((&rest
+                         keys
+                         &key
                          (initial-num-vars 0)
                          (initial-num-vars-z 0)
                          (initial-num-slots 256)
@@ -34,21 +55,14 @@ Also, all data on the diagram are lost when it exits the scope of WITH-MANAGER.
   values for the maximum size of the cache and for the limit for fast
   unique table growth based on the available memory.
 "
-  `(let ((*manager*
-          (make-manager
-           :pointer
-           (cudd-init ,initial-num-vars
-                      ,initial-num-vars-z
-                      ,initial-num-slots
-                      ,cache-size ,max-memory))))
-     (cudd-add-hook %mp% (callback before-gc-hook) :cudd-pre-gc-hook)
-     (cudd-add-hook %mp% (callback after-gc-hook) :cudd-post-gc-hook)
-     (cudd-add-hook %mp% (callback before-gc-hook) :cudd-pre-reordering-hook)
-     (cudd-add-hook %mp% (callback after-gc-hook) :cudd-post-reordering-hook)
-     (unwind-protect
-          (progn ,@body)
-       (cudd-quit %mp%)
-       (setf %mp% (cffi:null-pointer)))))
+
+  (declare (ignorable initial-num-vars
+                      initial-num-vars-z
+                      initial-num-slots
+                      cache-size
+                      max-memory))
+  `(let ((*manager* (manager-init ,@keys)))
+     ,@body))
 
 (defun info ()
   (uiop:with-temporary-file (:stream s :pathname path)
