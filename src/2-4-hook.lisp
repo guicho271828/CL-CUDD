6cbdb79478eb3dfcbff24c4c09e2cefcc02d7eac
Masataro Asai <guicho2.71828@gmail.com>
2018-03-05 12:13:35 +0900

    additional layer for debugging hooks

--- src/2-4-hook.lisp
+++ src/2-4-hook.lisp
@@ -33,40 +33,28 @@ The function takes one argument, a mode string (\"DD\", \"BDD\" or \"ZDD\").
 This list is managed by a lisp process, independently from Cudd_AddHook function.
 WITH-MANAGER macro registers 4 global hooks which in turn call the functions in this special variable.")
 
-(defcallback before-gc-hook :int ((dd :pointer) (mode :string) (data :pointer))
-  (declare (ignore data))
+(defun hook-runner (hooks dd mode)
   (let ((*manager* (make-manager :pointer dd)))
     (handler-case
-        (dolist (fn *before-gc-hook* 1)
+        (dolist (fn hooks 1)
           (funcall fn mode))
       (error ()
         0))))
+
+(defcallback before-gc-hook :int ((dd :pointer) (mode :string) (data :pointer))
+  (declare (ignore data))
+  (hook-runner *before-gc-hook* dd mode))
 
 (defcallback after-gc-hook :int ((dd :pointer) (mode :string) (data :pointer))
   (declare (ignore data))
-  (let ((*manager* (make-manager :pointer dd)))
-    (handler-case
-        (dolist (fn *after-gc-hook*)
-          (funcall fn mode))
-      (error ()
-        0))))
+  (hook-runner *after-gc-hook* dd mode))
 
 (defcallback before-reordering-hook :int ((dd :pointer) (mode :string) (data :pointer))
   (declare (ignore data))
-  (let ((*manager* (make-manager :pointer dd)))
-    (handler-case
-        (dolist (fn *before-reordering-hook*)
-          (funcall fn mode))
-      (error ()
-        0))))
+  (hook-runner *before-reordering-hook* dd mode))
 
 (defcallback after-reordering-hook :int ((dd :pointer) (mode :string) (data :pointer))
   (declare (ignore data))
-  (let ((*manager* (make-manager :pointer dd)))
-    (handler-case
-        (dolist (fn *after-reordering-hook*)
-          (funcall fn mode))
-      (error ()
-        0))))
+  (hook-runner *after-reordering-hook* dd mode))
 
 (setf *manager* (manager-init))
